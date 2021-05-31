b07b7ada11579cbc100194f59a0b85366d1a92e1
designC <znehraks@gmail.com>
2021-05-31 16:19:10 +0900

    just code finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,37 @@
 import React, { Component } from "react";
 import styled from "styled-components";
+import { Form, Input } from "./Input";
+import Number from "./Number";
 
 interface IState {
   counter: number;
+  name: string;
 }
 
 class App extends Component<{}, IState> {
   state = {
     counter: 0,
+    name: "",
   };
   render() {
-    const { counter } = this.state;
+    const { counter, name } = this.state;
     return (
       <div>
-        {counter} <button onClick={this.add}>Add</button>
+        <Form onFormSubmit={this.onFormSubmit}>
+          <Input value={name} onChange={this.onChange} />
+        </Form>
+        <Number count={counter} /> <button onClick={this.add}>Add</button>
       </div>
     );
   }
+  onChange = (event: React.SyntheticEvent<HTMLInputElement>) => {
+    console.log(event.target);
+  };
+
+  onFormSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+  };
+
   add = (): void => {
     // this.setState({counter:'hello'})
     this.setState((prev) => {
