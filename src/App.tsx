7529a4a5ba166216ec08e45e68a0deb7b77aafbc
designC <znehraks@gmail.com>
2021-05-31 14:51:51 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+
+interface IState {
+  counter: number;
+}
+
+class App extends Component<{}, IState> {
+  state = {
+    counter: 0,
+  };
+  render() {
+    const { counter } = this.state;
+    return (
+      <div>
+        {counter} <button onClick={this.add}>Add</button>
+      </div>
+    );
+  }
+  add = (): void => {
+    // this.setState({counter:'hello'})
+    this.setState((prev) => {
+      return {
+        counter: prev.counter + 1,
+      };
+    });
+  };
+}
+
+export default App;
